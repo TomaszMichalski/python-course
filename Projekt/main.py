f94b1877d57dab5da848067bfbd1333618247716
TomaszMichalski <tomasz.michalski19@gmail.com>
2018-04-17 19:54:51 +0200

    groups processing rework

--- Projekt/main.py
+++ Projekt/main.py
@@ -5,7 +5,7 @@ unary_operators = "!"
 
 #Get distinct list of variable names in expression
 def parse_var_names(e):
-    result = re.findall(r'[a-zA-z_]\w*', e) #variable may contain alphanumeric characters, but must start with a letter or underscore
+    result = re.findall(r'[a-zA-Z_]\w*', e) #variable may contain alphanumeric characters, but must start with a letter or underscore
     return list(set(result))
 
 #Get expression sides seprator index
@@ -34,24 +34,6 @@ def get_expression_right_side(e):
 
 #Validate the expression
 def validate(e):
-    #first level of validation
-    number_eq = e.count("=")
-    number_impl = e.count(">")
-    if(number_eq > 1 or number_impl > 1): #simple syntax check
-        return False
-    elif((number_eq == 1 and number_impl == 0) or (number_eq == 0 and number_impl == 1)): #one occurence of "=" or ">" should be present, but not both
-        if(validate_side(get_expression_left_side(e)) and validate_side(get_expression_right_side(e))): #second level of validation
-            return True
-        else:
-            return False
-    elif(get_separator_index(e) == -1 and validate_side(e)): #this means that expression has no "=" or ">" operator
-        return True
-    else:
-        return False
-
-#Validate left or right side of ">" / "=" expresssion
-#Or just expression without these operators
-def validate_side(e):
     state = "get_var_or_unary_op" #initial state
     br = 0 #brackets counter
     e_simplified = re.sub(r'\w+', 'v', e) #simplify the expression - we only need to know if there is a variable - handles 0's and 1' as well
@@ -141,12 +123,20 @@ def evaluate(e, var_list, binary_input):
         elif(token in unary_operators): #if token is a unary operator
             operator_stack.append(token) #push it on the operator stack
         elif(token in binary_operators): #if token is a binary operator
-            while operator_stack and operator_stack[-1] in unary_operators: #while there are operators of higher precedence
-                #apply them
-                operand = operand_stack.pop()
-                operator = operator_stack.pop()
-                ev = evaluate_unary_operator(operator, operand)
-                operand_stack.append(ev)
+            while operator_stack: #while there are operators of higher precedence, apply them
+                if operator_stack[-1] in unary_operators:
+                    operand = operand_stack.pop()
+                    operator = operator_stack.pop()
+                    ev = evaluate_unary_operator(operator, operand)
+                    operand_stack.append(ev)
+                elif operator_stack[-1] == "&":
+                    right = operand_stack.pop()
+                    left = operand_stack.pop()
+                    operator = operator_stack.pop()
+                    ev = evaluate_binary_operator(left, operator, right)
+                    operand_stack.append(ev)
+                else:
+                    break
             operator_stack.append(token)
         elif(token == "("):
             operator_stack.append("(")
@@ -180,19 +170,6 @@ def evaluate(e, var_list, binary_input):
     result = operand_stack.pop()
     return result
 
-#Evaluate full expression
-#Expression is considered full if it has left and right side
-def evaluate_full(e, var_list, binary_input):
-    if(get_separator_index(e) == -1): #no "=" or ">" operator
-        return evaluate(e, var_list, binary_input)
-    else:
-        #get both sides of expression and transform them to RPN
-        left_side = get_expression_left_side(e)
-        right_side = get_expression_right_side(e)
-        #get operator to know how to evaluate the full expression
-        operator = e[get_separator_index(e)]
-        return evaluate_binary_operator(evaluate(left_side, var_list, binary_input), operator, evaluate(right_side, var_list, binary_input))
-
 #Return set of values which (in a form of binary input where n-th bit represents 
 #value of n-th variable in given variable list) evaluate the expression to True
 def get_expression_true_list(e):
@@ -201,8 +178,9 @@ def get_expression_true_list(e):
     result = []
     for i in range (0, 2 ** bits): #for every possible input bit values
         binary_input = dec_to_bin(i, bits)
-        if(evaluate_full(e, var_list, binary_input) == "1"):
+        if(evaluate(e, var_list, binary_input) == "1"):
             result.append(binary_input)
+    print("true list: " + str(result))
     return result
 
 #Counts "1" occurences in given input string
@@ -235,22 +213,40 @@ def combine(first, second):
 
 #Processes Quine-McCluskey groups until no more combinations can be performed
 def process_groups(groups):
-    changed = False #flag that indicates if any change has been made this time
+    print("Before: " + str(groups))
+    new_groups = [] #list of lists of processsed input
+    used_input = [] #this will mark if input in pre-processed groups was used
     groups_quantity = len(groups)
-    for i in range(0, groups_quantity - 1): #for every groups except the last one
-        for first in groups[i]: #process every item in this group
-            for second in groups[i + 1]: #with every item in the next group
-                combination = combine(first, second)
-                if combination: #if the combination result exists
-                    #remove both
-                    groups[i].remove(first)
-                    groups[i + 1].remove(second)
-                    #and add the result
-                    groups[i].append(combination)
-                    changed = True
-                    break
+    for i in range(0, groups_quantity):
+        new_groups.append([])
+        used_input.append([])
+        for j in range(0, len(groups[i])):
+            used_input[i].append(False)
+    #now let's process the groups!
+    changed = False #flag that will indicate when to stop processing
+    for i in range(0, groups_quantity - 1): #for every group except the last one
+        for item_index in range(0, len(groups[i])): #for every item in i-th group
+            for item2_index in range(0, len(groups[i+1])): #for every item in i+1-th group
+                comb = combine(groups[i][item_index], groups[i + 1][item2_index]) #try to combine them
+                if comb: #if comb is not an empty string, so the merging was successful
+                    new_groups[count_ones(comb)].append(comb) #append it to proper new group
+                    used_input[i][item_index] = True #mark as used
+                    used_input[i + 1][item2_index] = True #mark as used
+                    changed = True #note that change has been made
+    print(str(changed))
+    #another round of processing has finished
+    for i in range(0, groups_quantity): #check if there are unused inputs - they need to be saved
+        for item_index in range(0, len(groups[i])):
+            if(used_input[i][item_index] == False): #if the input is unused
+                new_groups[count_ones(groups[i][item_index])].append(groups[i][item_index]) #append it as it was to the new group
+    groups = new_groups #we can forget about the used ones
+    print("After: " + str(groups))
+    #now it's time to erase duplicates in each group
+    for i in range(0, groups_quantity): #process every group
+        groups[i] = list(set(groups[i])) #make set out of the list, then make it a list 
     if changed:
-        process_groups(groups)
+        groups = process_groups(groups) #if there was a change, call processing again
+    return groups
 
 #Maps the given binary grouping result with variable list, creating minimalisation result element
 def get_result_element(var_list, bits):
@@ -262,10 +258,18 @@ def get_result_element(var_list, bits):
             result += " " + var_list[i] + "'"
     return result[1:] #just to omit the space at the beggining
 
+#Returns number of "-" in grouped input
+def get_placeholders_num(grouped_input):
+    res = 0
+    for token in grouped_input:
+        if(token == "-"):
+            res += 1
+    return res
+
 #Generate possible values out of grouped binary input
 def generate_binary_input(grouped_input):
     gen = [grouped_input]
-    while gen and len(gen) != len(grouped_input):
+    while gen and len(gen) != 2 ** get_placeholders_num(grouped_input):
         current = gen[0]
         for i in range(0, len(current)):
             if(current[i] == "-"):
@@ -275,8 +279,43 @@ def generate_binary_input(grouped_input):
                 break
     return gen
 
+#Initialize chart of width = length of list of input that cause the expression to evaluate to true
+#heigth = number of items after first stage minimalisation (process_groups)
+def initialize_chart(true_list, items):
+    chart = []
+    for x in range (len(items)):
+        chart.append([])
+        for y in range (len(true_list)):
+            chart[x].append(0)
+    #now the chart is filled with 0's
+    #fill 1's in correct places
+    for item_index in range (len(items)): #process each item
+        for input in generate_binary_input(items[item_index]): #for every input it generates (like '-1' generates '01' and '11')
+            for true_list_index in range (len(true_list)): #process each true-evaluating input
+                if(input == true_list[true_list_index]): #if item generates the true-evaluating input
+                    chart[item_index][true_list_index] = 1
+    return chart
+
+#Return the essential primes using given chart, variable names list and items (like '1-00) list
+def get_essential_primes(chart, var_list, items):
+    print(str(var_list))
+    primes = []
+    for column in range(0, len(chart[0])): #length of every chart row is the same, as equal to number of columns
+        count = 0 #number of 1's in column
+        for row in range(0, len(chart)): #number of rows
+            if(chart[row][column] == 1):
+                count += 1
+                essential_row = row
+
+        if(count == 1):
+            primes.append(get_result_element(var_list, items[essential_row]))
+            chart[essential_row][column] = 0 #erase the essential prime column from the chart so it will be all 0's
+
+    return list(set(primes))
+
 #TODO dodać tą tabelkę z wykreślaniem
 def minimalize(e, var_list, true_list):
+    print(str(var_list))
     if(len(true_list) == 0): #always evaluates to 0
         return "0"
     if(len(true_list) == 2 ** len(var_list)): #always evaluates to 1
@@ -287,14 +326,23 @@ def minimalize(e, var_list, true_list):
         groups.append([])
     for input in true_list: #fill the groups
         groups[count_ones(input)].append(input)
-    process_groups(groups)
+    groups = process_groups(groups)
+    print("Back in minimalize: " + str(groups))
     result = ""
+    #flatten the groups list of lists to a one-dimension list
+    items = []
+    #get the initialized chart
     for group in groups:
         for item in group:
+            items.append(item)
             if result:
                 result += " + " + get_result_element(var_list, item)
             else:
                 result = get_result_element(var_list, item)
+    chart = initialize_chart(true_list, items)
+    print(str(chart))
+    #get essential primes using the chart
+    essential_primes = get_essential_primes(chart, var_list, items)
     return result
             
 #Run the Quine-McCluskey algorithm for given expression
@@ -305,6 +353,7 @@ def quine_mccluskey(e):
         print("Expression is invalid")
 
 def main():
+    print(evaluate_binary_operator("0", ">", "1"))
     expression = input("Expression: ")
     quine_mccluskey(expression)
 

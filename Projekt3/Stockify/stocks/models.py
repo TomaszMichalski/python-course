95721ffa19f34f78c68e1c22d2a6dcfd27b286c7
TomaszMichalski <tomasz.michalski19@gmail.com>
2018-05-27 14:14:45 +0200

    Initial db rework

--- Projekt3/Stockify/stocks/models.py
+++ Projekt3/Stockify/stocks/models.py
@@ -1,18 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Stock(models.Model):
     name = models.CharField(max_length=10)
-
-class User(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField(max_length=30)
-    wallet = models.IntegerField()
 
 class Transaction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
     date = models.DateTimeField()
     amount = models.IntegerField()
-    rate = models.IntegerField()
+    value = models.IntegerField()
+    cost = models.IntegerField()
+    balance = models.IntegerField()
+
+class UserStock(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    stocks = models.IntegerField()
